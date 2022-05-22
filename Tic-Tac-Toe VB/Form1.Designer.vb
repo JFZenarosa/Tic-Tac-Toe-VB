f02deb8a81bb3a6ad1ba0b3b06e099ccf551e341
JFZenarosa <jaireh62@gmail.com>
2022-05-22 14:27:20 +0800

    added some fefature whenever a line wins the game it will higlight the winning line by changing the button color

--- Tic-Tac-Toe VB/Form1.Designer.vb
+++ Tic-Tac-Toe VB/Form1.Designer.vb
@@ -40,98 +40,108 @@ Partial Class TTTform
         '
         'btnA1
         '
+        Me.btnA1.BackColor = System.Drawing.Color.Gainsboro
         Me.btnA1.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btnA1.Location = New System.Drawing.Point(13, 39)
         Me.btnA1.Name = "btnA1"
         Me.btnA1.Size = New System.Drawing.Size(95, 96)
         Me.btnA1.TabIndex = 0
         Me.btnA1.TabStop = False
-        Me.btnA1.UseVisualStyleBackColor = True
+        Me.btnA1.UseVisualStyleBackColor = False
         '
         'btnB1
         '
+        Me.btnB1.BackColor = System.Drawing.Color.Gainsboro
         Me.btnB1.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btnB1.Location = New System.Drawing.Point(114, 39)
         Me.btnB1.Name = "btnB1"
         Me.btnB1.Size = New System.Drawing.Size(95, 96)
         Me.btnB1.TabIndex = 1
         Me.btnB1.TabStop = False
-        Me.btnB1.UseVisualStyleBackColor = True
+        Me.btnB1.UseVisualStyleBackColor = False
         '
         'btnC1
         '
+        Me.btnC1.BackColor = System.Drawing.Color.Gainsboro
         Me.btnC1.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btnC1.Location = New System.Drawing.Point(215, 39)
         Me.btnC1.Name = "btnC1"
         Me.btnC1.Size = New System.Drawing.Size(95, 96)
         Me.btnC1.TabIndex = 2
         Me.btnC1.TabStop = False
-        Me.btnC1.UseVisualStyleBackColor = True
+        Me.btnC1.UseVisualStyleBackColor = False
         '
         'btnA2
         '
+        Me.btnA2.BackColor = System.Drawing.Color.Gainsboro
         Me.btnA2.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btnA2.Location = New System.Drawing.Point(13, 141)
         Me.btnA2.Name = "btnA2"
         Me.btnA2.Size = New System.Drawing.Size(95, 96)
         Me.btnA2.TabIndex = 3
         Me.btnA2.TabStop = False
-        Me.btnA2.UseVisualStyleBackColor = True
+        Me.btnA2.UseVisualStyleBackColor = False
         '
         'btnB2
         '
+        Me.btnB2.BackColor = System.Drawing.Color.Gainsboro
         Me.btnB2.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btnB2.Location = New System.Drawing.Point(114, 141)
         Me.btnB2.Name = "btnB2"
         Me.btnB2.Size = New System.Drawing.Size(95, 96)
         Me.btnB2.TabIndex = 4
         Me.btnB2.TabStop = False
-        Me.btnB2.UseVisualStyleBackColor = True
+        Me.btnB2.UseVisualStyleBackColor = False
         '
         'btnC2
         '
+        Me.btnC2.BackColor = System.Drawing.Color.Gainsboro
         Me.btnC2.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btnC2.Location = New System.Drawing.Point(215, 141)
         Me.btnC2.Name = "btnC2"
         Me.btnC2.Size = New System.Drawing.Size(95, 96)
         Me.btnC2.TabIndex = 5
         Me.btnC2.TabStop = False
-        Me.btnC2.UseVisualStyleBackColor = True
+        Me.btnC2.UseVisualStyleBackColor = False
         '
         'btnA3
         '
+        Me.btnA3.BackColor = System.Drawing.Color.Gainsboro
         Me.btnA3.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btnA3.Location = New System.Drawing.Point(13, 243)
         Me.btnA3.Name = "btnA3"
         Me.btnA3.Size = New System.Drawing.Size(95, 96)
         Me.btnA3.TabIndex = 6
         Me.btnA3.TabStop = False
-        Me.btnA3.UseVisualStyleBackColor = True
+        Me.btnA3.UseVisualStyleBackColor = False
         '
         'btnB3
         '
+        Me.btnB3.BackColor = System.Drawing.Color.Gainsboro
         Me.btnB3.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btnB3.Location = New System.Drawing.Point(114, 243)
         Me.btnB3.Name = "btnB3"
         Me.btnB3.Size = New System.Drawing.Size(95, 96)
         Me.btnB3.TabIndex = 7
         Me.btnB3.TabStop = False
-        Me.btnB3.UseVisualStyleBackColor = True
+        Me.btnB3.UseVisualStyleBackColor = False
         '
         'btnC3
         '
+        Me.btnC3.BackColor = System.Drawing.Color.Gainsboro
         Me.btnC3.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btnC3.Location = New System.Drawing.Point(215, 243)
         Me.btnC3.Name = "btnC3"
         Me.btnC3.Size = New System.Drawing.Size(95, 96)
         Me.btnC3.TabIndex = 8
         Me.btnC3.TabStop = False
-        Me.btnC3.UseVisualStyleBackColor = True
+        Me.btnC3.UseVisualStyleBackColor = False
         '
         'MenuStrip1
         '
         Me.MenuStrip1.BackColor = System.Drawing.SystemColors.ActiveCaption
         Me.MenuStrip1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.MenuStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Visible
         Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FilesToolStripMenuItem})
         Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
         Me.MenuStrip1.Name = "MenuStrip1"
@@ -151,13 +161,13 @@ Partial Class TTTform
         'NewGameToolStripMenuItem
         '
         Me.NewGameToolStripMenuItem.Name = "NewGameToolStripMenuItem"
-        Me.NewGameToolStripMenuItem.Size = New System.Drawing.Size(180, 26)
+        Me.NewGameToolStripMenuItem.Size = New System.Drawing.Size(157, 26)
         Me.NewGameToolStripMenuItem.Text = "New Game"
         '
         'ExitToolStripMenuItem
         '
         Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
-        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(180, 26)
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(157, 26)
         Me.ExitToolStripMenuItem.Text = "Exit"
         '
         'TTTform
--- Tic-Tac-Toe VB/Form1.vb
+++ Tic-Tac-Toe VB/Form1.vb
@@ -27,27 +27,44 @@
 
         If btnA1.Text = btnA2.Text And btnA2.Text = btnA3.Text And Not btnA1.Enabled Then
             theWinner = True
-
+            btnA1.BackColor = System.Drawing.Color.AliceBlue
+            btnA2.BackColor = System.Drawing.Color.AliceBlue
+            btnA3.BackColor = System.Drawing.Color.AliceBlue
         ElseIf btnB1.Text = btnB2.Text And btnB2.Text = btnB3.Text And Not btnB1.Enabled Then
             theWinner = True
-
+            btnB1.BackColor = System.Drawing.Color.AliceBlue
+            btnB2.BackColor = System.Drawing.Color.AliceBlue
+            btnB3.BackColor = System.Drawing.Color.AliceBlue
         ElseIf btnC1.Text = btnC2.Text And btnC2.Text = btnC3.Text And Not btnC1.Enabled Then
             theWinner = True
-
+            btnC1.BackColor = System.Drawing.Color.AliceBlue
+            btnC2.BackColor = System.Drawing.Color.AliceBlue
+            btnC3.BackColor = System.Drawing.Color.AliceBlue
         ElseIf btnA1.Text = btnB2.Text And btnB2.Text = btnC3.Text And Not btnA1.Enabled Then
             theWinner = True
-
+            btnA1.BackColor = System.Drawing.Color.AliceBlue
+            btnB2.BackColor = System.Drawing.Color.AliceBlue
+            btnC3.BackColor = System.Drawing.Color.AliceBlue
         ElseIf btnC1.Text = btnB2.Text And btnB2.Text = btnA3.Text And Not btnC1.Enabled Then
             theWinner = True
-
+            btnC1.BackColor = System.Drawing.Color.AliceBlue
+            btnB2.BackColor = System.Drawing.Color.AliceBlue
+            btnA3.BackColor = System.Drawing.Color.AliceBlue
         ElseIf btnA1.Text = btnB1.Text And btnB1.Text = btnC1.Text And Not btnA1.Enabled Then
             theWinner = True
-
+            btnA1.BackColor = System.Drawing.Color.AliceBlue
+            btnB1.BackColor = System.Drawing.Color.AliceBlue
+            btnC1.BackColor = System.Drawing.Color.AliceBlue
         ElseIf btnA2.Text = btnB2.Text And btnB2.Text = btnC2.Text And Not btnA2.Enabled Then
             theWinner = True
-
+            btnA2.BackColor = System.Drawing.Color.AliceBlue
+            btnB2.BackColor = System.Drawing.Color.AliceBlue
+            btnC2.BackColor = System.Drawing.Color.AliceBlue
         ElseIf btnA3.Text = btnB3.Text And btnB3.Text = btnC3.Text And Not btnA3.Enabled Then
             theWinner = True
+            btnA3.BackColor = System.Drawing.Color.AliceBlue
+            btnB3.BackColor = System.Drawing.Color.AliceBlue
+            btnC3.BackColor = System.Drawing.Color.AliceBlue
         End If
 
         If theWinner Then
@@ -96,6 +113,8 @@
                 Dim a As Button = c
                 a.Enabled = True
                 a.Text = ""
+                a.BackColor = System.Drawing.Color.Gainsboro
+                a.TabStop = False
             Next
 
         Catch ex As Exception
